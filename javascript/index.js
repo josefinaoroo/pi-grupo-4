cc5ab8aaae63dfef9436bd72cd4a3b02a08b0475
josefinaoroo <jororastelli@udesa.edu.ar>
2025-11-13 14:16:33 -0300

    Termine lo que empecé ayer en la section1, y empecé la section2

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,6 @@
 let section1 = document.querySelector(".section1")
 console.log(section1)
-let productosSeccion1 = "";
+let productosSection1 = "";
 fetch('https://dummyjson.com/products')
 .then(function(response){
     return response.json()
@@ -14,19 +14,39 @@ fetch('https://dummyjson.com/products')
         <img src="./img/badbunny/totebag_bb.webp" alt="">
           <div class="info">
              <h3>${info.products[i].title}</h3>
-             <p>Totebag de Bad Bunny de su última gira</p>
-             <P>Precio: $36.000</P>
+             <p>${info.products[i] .description}</p>
+             <P>${info.products[i] .price}</P>
              <a href="productos.html">Ver detalle</a>
           </div>  
        </article>
      `    
     }
-    console.log(productosSeccion1)
-    section1.innerHTML = productosSeccion1;
+    console.log(productosSection1)
+    section1.innerHTML = productosSection1;
 }) .catch(function(error){
     console.error(error)
 })
 
+let section2 = document.querySelector(" .section2")
+console.log(section2)
+let productosSection2 = ""
 
+.then(function(info){
+    console.log(info);
+    for (let i = 0; i < info.products.length; i++){
+    console.log(info.products[i] .title)
+    productosSection2 +=
+        `<article class="productos">
+          <img src="./img/badbunny/remera_bb.webp" alt="">
+           <div class="info">
+             <h3>Remera Bad Bunny</h3>
+             <p>Remera de Bad Bunny del album DTMF</p>
+             <P>Precio: $55.000</P>
+             <a href="productos.html">Ver detalle</a>
+           </div> 
+         </article>
+            `
+    }
+)}
 
 
