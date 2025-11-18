7d1df994904d473f7360244733a66870ef4d5e96
josefinaoroo <jororastelli@udesa.edu.ar>
2025-11-17 23:05:25 -0300

    Termine el index html y js

--- javascript/index.js
+++ javascript/index.js
@@ -1,52 +1,50 @@
 let section1 = document.querySelector(".section1")
-console.log(section1)
 let productosSection1 = "";
-fetch('https://dummyjson.com/products')
+let section2 = document.querySelector(".section2")
+let productosSection2 = "";
+fetch('https://dummyjson.com/products/category/kitchen-accessories')
 .then(function(response){
     return response.json()
 })
 .then(function(info){
-    console.log(info);
-   for (let i = 0; i < info.products.length; i++) {
-    console.log(info.products [i].title)
-    productosSeccion1 += 
+  productosSection1 +=   `<h2>🖤 NUESTROS FAVORITOS DEL DÍA 🖤</h2>` 
+  for (let i = 0; i < info.products.length; i++) {
+    productosSection1 += 
      `<article class="productos">
-        <img src="./img/badbunny/totebag_bb.webp" alt="">
+        <img src=${info.products[i].images[0]} alt="${info.products[i].title}">
           <div class="info">
              <h3>${info.products[i].title}</h3>
              <p>${info.products[i] .description}</p>
              <P>${info.products[i] .price}</P>
-             <a href="productos.html">Ver detalle</a>
+             <a href="producto.html?id=${info.products[i].id}">Ver detalle</a>
           </div>  
        </article>
      `    
     }
-    console.log(productosSection1)
     section1.innerHTML = productosSection1;
 }) .catch(function(error){
     console.error(error)
 })
 
-let section2 = document.querySelector(" .section2")
-console.log(section2)
-let productosSection2 = ""
-
+fetch('https://dummyjson.com/products/category/sports-accessories')
+.then(function(response){
+    return response.json()
+})
 .then(function(info){
-    console.log(info);
-    for (let i = 0; i < info.products.length; i++){
-    console.log(info.products[i] .title)
-    productosSection2 +=
-        `<article class="productos">
-          <img src="./img/badbunny/remera_bb.webp" alt="">
-           <div class="info">
-             <h3>Remera Bad Bunny</h3>
-             <p>Remera de Bad Bunny del album DTMF</p>
-             <P>Precio: $55.000</P>
-             <a href="productos.html">Ver detalle</a>
-           </div> 
-         </article>
-            `
+      productosSection2 +=   `<h2>PRODUCTOS MÁS VENDIDOS</h2>`
+    for (let i = 0; i < info.products.length; i++) {
+       productosSection2 +=
+     `<article class="productos">
+        <img src=${info.products[i].images[0]} alt="${info.products[i].title}">
+          <div class="info">
+             <h3>${info.products[i].title}</h3>
+             <p>${info.products[i] .description}</p>
+             <P>${info.products[i] .price}</P>
+             <a href="producto.html?id=${info.products[i].id}">Ver detalle</a>
+          </div>  
+       </article>
+     `    
     }
-)}
-
+    section2.innerHTML = productosSection2;
+})
 
